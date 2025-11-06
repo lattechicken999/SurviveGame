9aae65f502f82e85c5119b23345aa7254ac9c614
lattechicken999 <whdtn218@gmail.com>
2025-11-06 17:20:09 +0900

    [feat] 맵 구조 등록, Cam 화면 전환 코드 추가

---
+++ Assets/Scripts/CCTVControl.cs
@@ -0,0 +1,35 @@
+﻿using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class CCTVControl : MonoBehaviour
+{
+    [SerializeField] CinemachineCamera[] cameras;
+
+    public void OnCam1()
+    {
+        SwitchCam(0);
+    }
+    public void OnCam2()
+    {
+        SwitchCam(1);
+    }
+    public void OnCam3()
+    {
+        SwitchCam(2);
+    }
+    public void OnCam4()
+    {
+        SwitchCam(3);
+    }
+
+    private void SwitchCam(int num)
+    {
+        for (int i = 0; i < cameras.Length; i++)
+        {
+            if(i == num)
+                cameras[i].Priority = 10;
+            else 
+                cameras[i].Priority = 8;
+        }
+    }
+}
