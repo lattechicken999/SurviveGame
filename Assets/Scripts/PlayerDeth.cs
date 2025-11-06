7c00be7faa7b5c23b132647de9d05ab92fd0b1a0
lattechicken999 <whdtn218@gmail.com>
2025-11-06 17:20:30 +0900

    [feat] 과제 완료 해버렸다이..

---
+++ Assets/Scripts/PlayerDeth.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerDeth : MonoBehaviour
+{
+    [SerializeField] private RawImage _endGameImage;
+
+    private void Start()
+    {
+
+        _endGameImage.enabled = false;
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Ghost"))
+        {
+            gameObject.SetActive(false);
+            _endGameImage.enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Vector2 _moving;
+    private Vector2 _cameraMove;
+    [SerializeField] float _speed;
+    [SerializeField] float _cameraSense;
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        _moving = ctx.ReadValue<Vector2>();
+    }
+
+    private void Update()
+    {
+        if (_moving != Vector2.zero)
+        {
+            //transform.Translate(new Vector3(_moving.x, 0, _moving.y).normalized * Time.deltaTime * _speed);
+            transform.Rotate(new Vector3(0, _moving.x, 0), Time.deltaTime * _cameraSense);
+            transform.Translate(new Vector3(0,0,_moving.y)*Time.deltaTime*_speed);
+        }
+    }
+}
